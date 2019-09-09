da42add1c1583e4a906e5249776f76b1edc20e47
phatdroid94 <phatdroid94@gmail.com>
2019-09-09 19:15:10 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentcameraandgallery29072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentcameraandgallery29072019/MainActivity.java
@@ -1,11 +1,16 @@
 package phamtanphat.ptp.khoaphamtraining.intentcameraandgallery29072019;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -33,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
                         Request_Code_Camera);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Request_Code_Camera){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,Request_Code_Camera);
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Request_Code_Camera && resultCode == RESULT_OK && data!= null){
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
